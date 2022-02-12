86d559a65a445c95434dfce1da19aacf617a8054
Xyrk <alex@xyrk.io>
2022-02-12 03:08:43 -0500

    added test sound file

--- backend-asterisk/testing_utils/restore_sound.sh
+++ backend-asterisk/testing_utils/restore_sound.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /var/lib/asterisk/sounds/covid2019/ &&
+cd /usr/share/asterisk/sounds/covid2019/ &&
 cp -rf covid_human.wav covid_recorded_human.wav
 #sox covid_recorded_human.wav -r 8000 -c1 covid_recorded_human.gsm
